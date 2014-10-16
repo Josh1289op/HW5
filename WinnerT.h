47d3e4139030a19f4c77b7dbf9cd185a4344b495
Josh1289op <Joshuakegley@gmail.com>
2014-10-15 23:33:59 -0400

    Winner Update works

--- WinnerT.h
+++ WinnerT.h
@@ -5,32 +5,82 @@ class WinnerT{
 private:
 	T * tree;
 	size_t size;
-	T eliminate_player_sig;
+	char eliminate_player_sig;
 public:
-	WinnerT(T * scores, int players, T elim){
+	WinnerT(T * scores, size_t players, char elim){
 		this->size = (players * 2);
+		std::cout << "SIZE: " << size << std::endl;
 		eliminate_player_sig = elim;
-		tree = new T[this->size];
+		tree = new T[this->size + 1];
 		
 
-		//figure out placement of array
-		for (int i = players; i < this->size; ++i){
+		//TODO: figure out placement of array
+		for (int i = players; i < (int)this->size + 1; ++i){
 			tree[i] = scores[i - players];
 		}
-
-		for (int = players - 1;)
-
+		for (int i = players - 1; i > 0; --i){
+			tree[i] = tree[i * 2] > tree[i * 2 + 1] ? tree[i * 2] : tree[i * 2 + 1];
+		}
 	}
 
 	~WinnerT(){
-
+		delete[] tree;
 	}
 
-	T winner(){ return eliminate_player; }
-	void eliminate_player(int player){ }
-	int get_size(){ return (int)(size - 1); }
-	bool empty(){ return false; }
-	void pretty_print(std::ostream&){}
+	
+	void update_player(int player, int new_score){
+		int position = (size / 2 - 1) + player;
+		bool updating = true;
+		tree[position] = new_score;
+		while (updating){
+			if (tree[position] <= tree[position / 2]){
+				//do nothing,
+				updating = false;
+				std::cout << tree[position] << " " << tree[position / 2] << std::endl;
+			}
+			else{
+				position = position / 2;
+				tree[position] = tree[position * 2] > tree[position * 2 + 1] ? tree[position * 2] : tree[position * 2 + 1];
+				std::cout << "UPDATING SCORE" << std::endl;
+				if (position == 1 ){
+					updating = false;
+				}
+			}
+		}
+	}
+
+	void eliminate_player(int player){
+		int position = (size / 2 - 1) + player;
+		bool updating = true;
+		if (tree[position] < tree[position / 2]){
+			//do nothing
+			updating = false;
+
+		}
+		tree[position] = NULL;
+	}
+
+
+
+
+	T winner(){ return tree[1]; }
+	int get_size(){ return (int)(size); }
+	bool empty(){ return size > 0 ? true : false; }
+	void pretty_print(std::ostream&){
+		//fake print for now;
+		for (int i = 0; i < (int)size; ++i){
+			if (i == 1){
+				std::cout << "Winner: " << " " << tree[i] << std::endl;
+			}
+			else if (tree[i] == NULL){
+				std::cout << eliminate_player_sig << std::endl;
+			}
+			else {
+				std::cout << tree[i] << std::endl;
+			}
+		}
+
+	}
 
 
 
--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 
 int main(){
 
-	int players[5] = { 1, 2, 3, 4, 5 };
+	int players[4] = { 1, 2, 3, 4 };
 
-	WinnerT<int>* lol = new WinnerT<int>(players, 5, 0);
-
-
+	WinnerT<int>* lol = new WinnerT<int>(players, (size_t)4, 'X');
 
 
 
-
-
-
+	lol->pretty_print(std::cout);
+	std::cout << std::endl << std::endl << std::endl;
+	lol->update_player(2, 1);
+	lol->eliminate_player(2);
+	std::cout << std::endl << std::endl << std::endl;
+	lol->pretty_print(std::cout);
 	return 0; 
 }
